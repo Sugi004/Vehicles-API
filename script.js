f328c3dfc05424e01e6acbabefd4bfd1abec41e7
sugi04 <sugishivam8@gmail.com>
2023-06-17 22:18:11 +0530

    new changes

--- script.js
+++ script.js
@@ -1,57 +1,18 @@
 const url = "https://vpic.nhtsa.dot.gov/api/vehicles/GetVehicleTypesForMake/"
 var apiUrl;  //Created an empty Variable to store the API URL when ser selects an option
 
-function selectedVehicle(){
-
-let selectedValue = document.getElementById("selectName").value
+function selectedVehicle(value){
 
 
 // Using if else if function to add the Vehicles of users choice in URL
 
-    if(selectedValue  == "mec"){
-        apiUrl = url + "merc" + "?format=json"
+   if(value !=="0"){
+        apiUrl = url + value + "?format=json"
         getVehicleData(apiUrl)
-        
     }
 
-    else if(selectedValue  == "honda"){
-        apiUrl = url + "honda" + "?format=json"
-        getVehicleData(apiUrl)
-       
-    }
-    else if(selectedValue  == "hyundai"){
-        apiUrl = url + "hyundai" + "?format=json"
-        getVehicleData(apiUrl)
-        
-    }
-    else if(selectedValue  == "jaguar"){
-        apiUrl = url + "jaguar" + "?format=json"
-        getVehicleData(apiUrl)
-       
-    }
-    else if(selectedValue  == "bugatti"){
-        apiUrl = url + "bugatti" + "?format=json"
-        getVehicleData(apiUrl)
-        
-    }
-    else if(selectedValue  == "tesla"){
-        apiUrl = url + "tesla" + "?format=json"
-        getVehicleData(apiUrl)
-       
-    }
-    else if(selectedValue  == "bmw"){
-        apiUrl = url + "bmw" + "?format=json"
-        getVehicleData(apiUrl)
-        
-    }
-
-    else if(selectedValue  == "mitsubishi"){
-        apiUrl = url + "mitsubishi" + "?format=json"
-        getVehicleData(apiUrl)
-        
-    }
-
-    else if(selectedValue == 0){
+    else
+    {
         constructData(0)
 
     }
@@ -77,38 +38,38 @@ async function getVehicleData(URL)   // The URLs from apiUrl is passed as a para
 
 function constructData(data) // This function creates the table when ever the user chooses an option
 {
-  
     let div = document.getElementById("table")
-    div.innerHTML = ""
-    let table = document.createElement("table")
-    table.setAttribute("class", "table table-striped table-bordered table-dark")
-    let thead = document.createElement("thead")
-    let th1 = document.createElement("th")
-    th1.innerHTML = "Make ID"
-    thead.appendChild(th1)
-    let th2 = document.createElement("th")
-    th2.innerHTML = "Make Name"
-    thead.appendChild(th2)
-    let th3 = document.createElement("th")
-    th3.innerHTML = "Vehicle Type"
-    thead.appendChild(th3)
-    table.appendChild(thead)
-    div.appendChild(table)
+        div.innerHTML = ""
+        let table = document.createElement("table")
+        table.setAttribute("class", "table table-striped table-bordered table-dark")
+        let thead = document.createElement("thead")
+        let th1 = document.createElement("th")
+        th1.innerHTML = "Make ID"
+        thead.appendChild(th1)
+        let th2 = document.createElement("th")
+        th2.innerHTML = "Make Name"
+        thead.appendChild(th2)
+        let th3 = document.createElement("th")
+        th3.innerHTML = "Vehicle Type"
+        thead.appendChild(th3)
+        table.appendChild(thead)
+        div.appendChild(table)
 
-    let tbody = document.createElement("tbody")
-    tbody.innerHTML = ""
-    table.appendChild(tbody)
-
+        let tbody = document.createElement("tbody")
+        tbody.innerHTML = ""
+        table.appendChild(tbody)
+  
     if(data!==0){
-    data.forEach(e => {
-        let tr = document.createElement("tr")
-        tr.innerHTML = `<tr>
-                <td>${e.MakeId}</td>
-                <td>${e.MakeName}</td>
-                <td>${e.VehicleTypeName}</td>
-        </tr>`
-        tbody.appendChild(tr)
-    });
+        
+        data.forEach(e => {
+            let tr = document.createElement("tr")
+            tr.innerHTML = `<tr>
+                    <td>${e.MakeId}</td>
+                    <td>${e.MakeName}</td>
+                    <td>${e.VehicleTypeName}</td>
+            </tr>`
+            tbody.appendChild(tr)
+        });
 }
 else  // If the option is Select Car then it displays nothing
 {
